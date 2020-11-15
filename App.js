7b82f95d7a3ec616e2fc4d5dd4278d7445a512b1
sbin0819 <sbinha123@gmail.com>
2020-11-15 15:55:32 +0900

    api 키 제거

--- App.js
+++ App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
-
-// export default class extends React.Component {
-//   getLocation = async () => {
-//     const location = await Location.getCurrentPositionAsync();
-//     console.log(location);
-//   };
-//   componentDidMount() {
-//     this.getLocation();
-//   }
-//   render() {
-//     return <Loading />;
-//   }
-// }
+import axios from 'axios';
 
 export default function App() {
   const [isLoading, setIsLoading] = React.useState(true);
+  const getWeather = async (latitude, longitude) => {
+    try {
+      const response = await axios.get(
+        `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=`,
+      );
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   const getLocation = async () => {
     try {
       // 장소에 관한 허용이 필요함
@@ -26,6 +24,7 @@ export default function App() {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
       // Send to API and get weahter
+      getWeather(latitude, longitude);
       setIsLoading(false);
     } catch (err) {
       Alert.alert(`Can't find you.`, 'check your device');
